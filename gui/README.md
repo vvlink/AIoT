2372afda12a82038cddb378c398cf3642ad00ead
xiezuoru <8334180@qq.com>
2023-09-01 00:21:05 +0800

    谢作如
    更新

--- gui/README.md
+++ gui/README.md
@@ -1,2 +1,12 @@
-# AIoT
-一个物联网结合人工智能的教育项目
+# GUI界面编写
+
+使用PySimpleGUI库编写GUI界面。
+
+## 介绍
+
+顾名思义，PySimpleGUI是一个简单的GUI设计工具。PySimpleGUI的安装和其他库一样，使用pip命令即可。整个库很小，很快就能完成。参考命令如下：
+
+pip install  PySimpleGUI
+
+
+
--- gui/webgui_infer/webgui_infer_auto_cls.py
+++ gui/webgui_infer/webgui_infer_auto_cls.py
@@ -4,7 +4,6 @@ import remi
 import PySimpleGUIWeb as sg
 import BaseDeploy as bd
 import cv2  #pip install opencv-python
-import numpy as np #pip install numpy
 
 def my_inf(frame):
     model_path = 'cls_imagenet.onnx'
@@ -28,7 +27,6 @@ window = sg.Window('OpenCV实时图像处理',layout,size=(600, 500))
 cap = cv2.VideoCapture(1)
 while True:
     event, values = window.read(timeout=0, timeout_key='timeout')
-
     #实时读取图像，重设画面大小
     ret, frame = cap.read()
     imgSrc = cv2.resize(frame, (600,400))
@@ -42,6 +40,6 @@ while True:
     window['image'].update(data=imgbytes)
     if event in (None, '关闭'):
         break
-# 推出窗体
+# 退出窗体
 cap.release()
 window.close()
