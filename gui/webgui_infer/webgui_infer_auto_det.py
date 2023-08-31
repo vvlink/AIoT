d617a801205e58957bf9325248941dfbf2d1d5fb
xiezuoru <8334180@qq.com>
2023-08-31 22:16:45 +0800

    Update webgui_infer_auto_det.py

--- gui/webgui_infer/webgui_infer_auto_det.py
+++ gui/webgui_infer/webgui_infer_auto_det.py
@@ -4,19 +4,20 @@ import remi
 import PySimpleGUIWeb as sg
 import BaseDeploy as bd
 import cv2  #pip install opencv-python
-import numpy as np #pip install numpy
-from BaseDT.plot import imshow_det_bboxes
 
 model_path = 'det.onnx'
 model = bd(model_path)
 
 def my_inf(frame):
-    global model,class_name
+    global model
     res1, img = model.inference(frame,get_img='cv2')
+    # 转化推理结果
     res2 = model.print_result(res1)
-    if len(res2)==0:
+    if len(res2) == 0:
         return None,None
-    classes=[]
+    classes = []
+    print(res2)
+    # 提取预测结果
     for res in res2:
         classes.append(res['预测结果'])
     return str(classes),img
@@ -30,26 +31,25 @@ layout = [
   [sg.Text('推理结果：',key='res')]
 ]
 
-#窗口设计
+# 建立窗体
 window = sg.Window('OpenCV实时图像处理',layout,size=(600, 500))
-#打开内置摄像头
-cap = cv2.VideoCapture(1)
+#打开摄像头
+cap = cv2.VideoCapture(0)
 while True:
     event, values = window.read(timeout=0, timeout_key='timeout')
+    if event in (None, '关闭'):
+        break
     #实时读取图像，重设画面大小
     ret, frame = cap.read()
     imgSrc = cv2.resize(frame, (600,400))
     res, img = my_inf(frame)
-    #画面实时更新
+    # 实时更新画面
     if res:
-        print('推理结果为：',res)
         window['res'].update('推理结果：'+res)
         imgbytes = cv2.imencode('.png', img)[1].tobytes()
     else:
         imgbytes = cv2.imencode('.png', imgSrc)[1].tobytes()
     window['image'].update(data=imgbytes)
-    if event in (None, '关闭'):
-        break
 # 退出窗体
 cap.release()
 window.close()
